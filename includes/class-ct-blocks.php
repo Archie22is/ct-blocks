2d985a9dfdd658f1696ee31734db5ca79a695561
Thaophuong <thao@codetot.com>
2021-02-22 23:26:58 +0700

    Update block

--- includes/class-ct-blocks.php
+++ includes/class-ct-blocks.php
@@ -72,6 +72,8 @@ class Codetot_Base
     add_filter('ct_theme_block_parts_paths', array($this, 'load_block_parts_paths'));
 
     include_once CODETOT_BLOCKS_DIR . 'includes/helpers.php';
+    include_once CODETOT_BLOCKS_DIR . 'includes/acf.php';
+    include_once CODETOT_BLOCKS_DIR . 'includes/breadcrumbs.php';
 
     $this->load_dependencies();
 
