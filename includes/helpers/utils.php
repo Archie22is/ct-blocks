1e802a785445d70c00677a7a5337f3a02eee3c3a
Khoi Nguyen <khoi@codetot.com>
2021-02-22 16:28:52 +0700

    Wrap function to prevent duplicate

--- includes/helpers/utils.php
+++ includes/helpers/utils.php
@@ -67,47 +67,53 @@ if ( ! function_exists( 'the_block' ) ) {
     }
   }
 }
-/**
- * @param string $path_name
- * @return false|string
- */
-function get_block_part($path_name)
-{
-  ob_start();
-  the_block_part($path_name);
-  return ob_get_clean();
+
+if (!function_exists('get_block_part')) {
+  /**
+   * @param string $path_name
+   * @return false|string
+   */
+  function get_block_part($path_name)
+  {
+    ob_start();
+    the_block_part($path_name);
+    return ob_get_clean();
+  }
 }
 
-/**
- * @param string $path_name
- */
-function the_block_part($path_name)
-{
-  $available_paths = apply_filters('ct_theme_block_parts_paths', []);
 
-  $path = '';
+if (!function_exists('the_block_part')) {
+  /**
+   * @param string $path_name
+   */
+  function the_block_part($path_name)
+  {
+    $available_paths = apply_filters('ct_theme_block_parts_paths', []);
 
-  try {
-    foreach ($available_paths as $available_path) {
-      $file_name = $available_path . '/' . esc_html($path_name) . '.php';
+    $path = '';
 
-      if (file_exists($file_name)) {
-        $path = $file_name;
-      }
-    }
+    try {
+      foreach ($available_paths as $available_path) {
+        $file_name = $available_path . '/' . esc_html($path_name) . '.php';
 
-    if (!empty($path)) {
-      include($path);
-    } else {
-      if (WP_DEBUG) {
-        var_dump($available_paths);
+        if (file_exists($file_name)) {
+          $path = $file_name;
+        }
       }
 
-      throw new Exception(sprintf(__('Block part %s is not available.', 'codetot'), $path_name));
+      if (!empty($path)) {
+        include($path);
+      } else {
+        if (WP_DEBUG) {
+          var_dump($available_paths);
+        }
+
+        throw new Exception(sprintf(__('Block part %s is not available.', 'codetot'), $path_name));
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+      die();
     }
-  } catch (Exception $e) {
-    echo $e->getMessage();
-    die();
   }
 }
 
@@ -131,6 +137,7 @@ if ( ! function_exists( 'codetot_get_sub_fields' ) ) {
     return $output;
   }
 }
+
 if ( ! function_exists( 'codetot_svg' ) ) {
   function codetot_svg($name, $echo = true)
   {
@@ -156,118 +163,127 @@ if ( ! function_exists( 'codetot_svg' ) ) {
   }
 }
 
-function codetot_is_woocommerce_activated()
-{
-  return class_exists('WooCommerce');
-}
-
-/**
- * @param string $background_name
- * @return bool
- */
-function codetot_is_dark_background($background_name)
-{
-  return in_array($background_name, array('primary', 'secondary', 'dark', 'black'));
-}
-
-/**
- * @param $args
- * @param $origin_class
- * @param string $extra_class
- * @return string
- */
-function codetot_block_generate_class($args, $origin_class)
-{
-  $_class = array($origin_class);
-
-  foreach ($args as $key => $arg) {
-    $_class[$key] = _codetot_block_generate_class($key, $arg, $origin_class);
+if (!function_exists('codetot_is_woocommerce_activated')) {
+  function codetot_is_woocommerce_activated()
+  {
+    return class_exists('WooCommerce');
   }
-
-  return implode(' ', $_class);
 }
 
-/**
- * @param string $key
- * @param string $value
- * @param string $prefix_class
- * @return string
- */
-function _codetot_block_generate_class($key, $value, $prefix_class)
-{
+
+if (!function_exists('codetot_is_dark_background')) {
   /**
-   * Available args
-   *
-   * block_preset
-   * layout|main_layout
-   * content_layout
-   * header_alignment
-   * content_alignment
-   * background_type
-   * column
+   * @param string $background_name
+   * @return bool
    */
-
-  $_class = '';
-
-  switch ($key) {
-    case 'block_preset':
-      $_value = str_replace('preset-', '', $value);
-      $_class = sprintf('%s--preset-%s', $prefix_class, $_value);
-      break;
-
-    case 'main_layout':
-    case 'layout':
-      $_class = sprintf('%s--layout-%s', $prefix_class, $value);
-      break;
-
-    case 'content_layout':
-      $_class = sprintf('%s--content-layout-%s', $prefix_class, $value);
-      break;
-
-    case 'header_alignment':
-      $_class = sprintf('%s--header-alignment-%s', $prefix_class, $value);
-      break;
-
-    case 'content_alignment':
-      $_class = sprintf('%s--content-alignment-%s', $prefix_class, $value);
-      break;
-
-    case 'background_type':
-      $new_class = sprintf('bg-%s', $value);
-      $new_class .= $key == 'white' ? ' section' : sprintf(' %s--has-bg section-bg', $prefix_class);
-      $new_class .= codetot_is_dark_background($value) ? sprintf(' %s--dark-contract', $prefix_class) : sprintf(' %s--light-contract', $prefix_class);
-
-      $_class = $new_class;
-
-      break;
-
-    case 'hide_mobile':
-      $_class = 'section--hide-mobile';
-      break;
-
-    case 'column':
-      if (is_numeric((int) $value)) {
-        $number_class = sprintf(_n('%s-column', '%s-columns', $value), $value);
-      } else {
-        $number_class = $value - '-columns';
-      }
-
-      $_class = sprintf('%s--%s', $prefix_class, $number_class);
-      break;
-
-    case 'fullscreen':
-    case 'no_container':
-      if (isset($value) && $value) {
-        $_value = str_replace('_', '-', $key);
-        $_class = sprintf('%s--%s', $prefix_class, $_value);
-      }
-
-      break;
-
-    case 'class':
-      $_class = $value;
-      break;
+  function codetot_is_dark_background($background_name)
+  {
+    return in_array($background_name, array('primary', 'secondary', 'dark', 'black'));
   }
+}
 
-  return $_class;
+if (!function_exists('codetot_block_generate_class')) {
+  /**
+   * @param $args
+   * @param $origin_class
+   * @param string $extra_class
+   * @return string
+   */
+  function codetot_block_generate_class($args, $origin_class)
+  {
+    $_class = array($origin_class);
+
+    foreach ($args as $key => $arg) {
+      $_class[$key] = _codetot_block_generate_class($key, $arg, $origin_class);
+    }
+
+    return implode(' ', $_class);
+  }
+}
+
+if (!function_exists('_codetot_block_generate_class')) {
+  /**
+   * @param string $key
+   * @param string $value
+   * @param string $prefix_class
+   * @return string
+   */
+  function _codetot_block_generate_class($key, $value, $prefix_class)
+  {
+    /**
+     * Available args
+     *
+     * block_preset
+     * layout|main_layout
+     * content_layout
+     * header_alignment
+     * content_alignment
+     * background_type
+     * column
+     */
+
+    $_class = '';
+
+    switch ($key) {
+      case 'block_preset':
+        $_value = str_replace('preset-', '', $value);
+        $_class = sprintf('%s--preset-%s', $prefix_class, $_value);
+        break;
+
+      case 'main_layout':
+      case 'layout':
+        $_class = sprintf('%s--layout-%s', $prefix_class, $value);
+        break;
+
+      case 'content_layout':
+        $_class = sprintf('%s--content-layout-%s', $prefix_class, $value);
+        break;
+
+      case 'header_alignment':
+        $_class = sprintf('%s--header-alignment-%s', $prefix_class, $value);
+        break;
+
+      case 'content_alignment':
+        $_class = sprintf('%s--content-alignment-%s', $prefix_class, $value);
+        break;
+
+      case 'background_type':
+        $new_class = sprintf('bg-%s', $value);
+        $new_class .= $key == 'white' ? ' section' : sprintf(' %s--has-bg section-bg', $prefix_class);
+        $new_class .= codetot_is_dark_background($value) ? sprintf(' %s--dark-contract', $prefix_class) : sprintf(' %s--light-contract', $prefix_class);
+
+        $_class = $new_class;
+
+        break;
+
+      case 'hide_mobile':
+        $_class = 'section--hide-mobile';
+        break;
+
+      case 'column':
+        if (is_numeric((int) $value)) {
+          $number_class = sprintf(_n('%s-column', '%s-columns', $value), $value);
+        } else {
+          $number_class = $value - '-columns';
+        }
+
+        $_class = sprintf('%s--%s', $prefix_class, $number_class);
+        break;
+
+      case 'fullscreen':
+      case 'no_container':
+        if (isset($value) && $value) {
+          $_value = str_replace('_', '-', $key);
+          $_class = sprintf('%s--%s', $prefix_class, $_value);
+        }
+
+        break;
+
+      case 'class':
+        $_class = $value;
+        break;
+    }
+
+    return $_class;
+  }
 }
