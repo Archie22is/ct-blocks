b9b604296e69734fa5cf43fcc7090a61465f55e3
Hoang Huong <70555229+codetot-huong@users.noreply.github.com>
2021-03-22 13:44:55 +0700

    Update post grid

--- blocks/post-grid.php
+++ blocks/post-grid.php
@@ -7,7 +7,7 @@ $container = codetot_site_container();
 if ( !empty($query) ) : ?>
   <div class="<?php echo $_class; ?>">
     <div class="<?php echo $container; ?> post-grid__container">
-      <div class="grid post-grid__grid" data-aos="fade-up" data-aos-duration="800">
+      <div class="grid post-grid__grid">
         <?php while( $query->have_posts() ) : $query->the_post(); ?>
           <div class="f fdc grid__col post-grid__col">
             <?php the_block('post-card', array(
