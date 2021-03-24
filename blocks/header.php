cf9ed4d3b15c640d5d375fc238c1510c8da8f94d
Khoi Nguyen <khoi@codetot.com>
2021-03-24 17:27:55 +0700

    Remove all blocks which should be on ct-theme

--- blocks/header.php
+++
@@ -1,5 +0,0 @@
-<header id="masthead" class="<?php codetot_header_class(); ?>"
-        data-block="header"
->
-  <?php do_action('codetot_header'); ?>
-</header>
--- blocks/post-sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if(!empty($sidebar)) : ?>
-  <div class="post-sidebar">
-    <?php echo $sidebar; ?>
-  </div>
-<?php endif; ?>
