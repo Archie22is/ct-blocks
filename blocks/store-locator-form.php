2349e435f39c7f0b618592451d4956550ac97b8a
Thaophuong <thao@codetot.com>
2021-04-07 10:35:35 +0700

    Update Store Locator

--- blocks/store-locator-form.php
+++ blocks/store-locator-form.php
@@ -10,7 +10,7 @@ $province_args = array(
 
 $category_province_store = get_terms($category_name, $province_args);
 
-$levels = ['area', 'province', 'district'];
+$levels = ['country', 'province', 'district'];
 ?>
 
 <div class="store-locator-form js-store-locator-form">
@@ -19,21 +19,30 @@ $levels = ['area', 'province', 'district'];
     <div class="store-locator-form__<?php echo $level ?> select-wrapper">
       <select class="js-<?php echo $level ?> store-locator-form__select">
         <?php
+        if ($level === 'country') {
+          printf('<option value="" disabled hidden>Choose Country</option>');
+        } elseif ($level === 'province') {
+          printf('<option value="" disabled hidden>Choose Province</option>');
+        } else {
+          printf('<option value="" disabled hidden>Choose District</option>');
+        }
+        ?>
+        <?php
         foreach ($category_province_store as $category) : ?>
           <?php
           $child_args = array('hide_empty' => false, 'parent' => $category->term_id);
           $child_cat = get_terms($category_name, $child_args);
-          if ($level === 'area') {
+          if ($level === 'country') {
             printf('<option value="%1$s">%2$s</option>', $category->term_id, $category->name);
           } else {
             foreach ($child_cat as $child_term) {
               if ($level === 'province') {
-                printf('<option class="js-province-%1$s" value="%2$s">%3$s</option>', $category->term_id, $child_term->term_id, $child_term->name);
+                printf('<option class="js-province-%1$s" data-country="%1$s" value="%2$s">%3$s</option>', $category->term_id, $child_term->term_id, $child_term->name);
               } else {
                 $district_args = array('hide_empty' => false, 'parent' => $child_term->term_id);
                 $district_cat = get_terms($category_name, $district_args);
                 foreach ($district_cat as $district_term) {
-                  printf('<option class="js-district-%1$s" value="%2$s">%3$s</option>', $child_term->term_id, $district_term->term_id, $district_term->name);
+                  printf('<option class="js-district-%1$s" data-province="%1$s" value="%2$s">%3$s</option>', $child_term->term_id, $district_term->term_id, $district_term->name);
                 }
               }
             }
--- blocks/store-locator.php
+++ blocks/store-locator.php
@@ -1,5 +1,5 @@
 <?php
-$_class = 'store-locator';
+$_class = 'store-locator js-store-locator';
 $container = codetot_site_container();
 ?>
 <section class="<?php echo $_class; ?>" data-ct-block="store-locator">
@@ -8,10 +8,13 @@ $container = codetot_site_container();
       <?php the_block('store-locator-form'); ?>
     </div>
   </div>
+
+
   <div class="default-section__main">
     <div class="<?php echo $container; ?> default-section__container default-section__container--main">
+      <div class="js-no-result sidebar-section__no-result">There is no matching map</div>
       <div class="sidebar-section__container">
-        <div class="grid sidebar-section__block-grid">
+        <div class="grid sidebar-section__block-grid js-sidebar-section">
           <div class="grid__col sidebar-section__block sidebar-section__block--sidebar">
             <div class="sidebar-section__inner">
               <?php the_block('store-locator-item'); ?>
