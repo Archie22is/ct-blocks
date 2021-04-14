44f756495d8590850b1aa489797b78454c889e20
Khoi Nguyen <khoi@codetot.com>
2021-04-14 18:02:59 +0700

    Update blocks svg icons and preview image url

--- includes/blocks/breadcrumbs.php
+++ includes/blocks/breadcrumbs.php
@@ -22,12 +22,12 @@ class Codetot_Block_Breadcrumbs extends Codetot_Base_Block implements Codetot_Ba
    */
   public $fields;
 
-    /**
+  /**
    * @var string
    */
   public $svg_icon;
 
-    /**
+  /**
    * @var string
    */
   public $preview_image_url;
@@ -44,8 +44,9 @@ class Codetot_Block_Breadcrumbs extends Codetot_Base_Block implements Codetot_Ba
    *
    * @return Codetot_Block_Breadcrumbs
    */
-  public final static function instance() {
-    if ( is_null( self::$instance ) ) {
+  public final static function instance()
+  {
+    if (is_null(self::$instance)) {
       self::$instance = new self();
     }
     return self::$instance;
@@ -59,9 +60,8 @@ class Codetot_Block_Breadcrumbs extends Codetot_Base_Block implements Codetot_Ba
     $this->fields = ['class'];
 
     ob_start();
-    echo '<svg id="breadcrumbs" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 1h2v22h-2v-11h-8l-3-3 3-3h8v-5zm13 5l-3-3h-7v6h7l3-3zm-10 5v4h6l2-2-2-2h-6z"/></svg>';
+    echo '<svg id="breadcrumbs" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 1h2v22h-2v-11h-8l-3-3 3-3h8v-5zm13 5l-3-3h-7v6h7l3-3zm-10 5v4h6l2-2-2-2h-6z" /></svg>';
     $this->svg_icon = ob_get_clean();
-
     $this->preview_image_url = CODETOT_BLOCKS_PLUGIN_URI . '/assets/img/breadcrumbs.jpg';
 
     parent::__construct();
