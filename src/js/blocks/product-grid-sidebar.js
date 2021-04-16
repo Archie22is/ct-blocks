5d8faf482ca3efc2fb80c36d4f15a4a67b05adc0
Thaophuong <thao@codetot.com>
2021-04-16 18:02:43 +0700

    Update Product Grid Sidebar

--- src/js/blocks/product-grid-sidebar.js
+++ src/js/blocks/product-grid-sidebar.js
@@ -1,10 +1,13 @@
-import { select, toggleClass, on } from 'lib/dom'
+import { select, toggleClass, on, addClass } from 'lib/dom'
+import { isMobile } from 'lib/utils'
 
 const VISIBLE_CLASS = 'sidebar-category-visible'
+const VISIBLE_MENU = 'menu-visible'
 
 export default el => {
   const button = select('.js-trigger', el)
   const content = select('.js-sidebar-block', el)
+  const headerEl = select('.product-grid-sidebar__header', el)
 
   if (button) {
     on(
@@ -14,5 +17,13 @@ export default el => {
       },
       button
     )
+  }
+
+  if (headerEl.offsetWidth < content.offsetWidth) {
+    addClass(VISIBLE_MENU, content.parentNode)
+  }
+
+  if (isMobile.any()) {
+    addClass(VISIBLE_MENU, content.parentNode)
   }
 }
