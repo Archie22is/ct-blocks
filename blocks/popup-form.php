47e25bc1c70a2f9f32e4d27184a7bf881f2c6658
Khoi Nguyen <khoi@codetot.com>
2021-07-02 17:27:51 +0700

    Refactor popup form to use existing modal class js

--- blocks/popup-form.php
+++ blocks/popup-form.php
@@ -1,50 +1,25 @@
 <?php
 
-// 'class',
-// 'block_preset',
-// 'select_form',
-// 'action_attribute',
-// 'label',
-// 'title',
-// 'description'
-$container_class = codetot_site_container();
-
-$prefix_class = 'popup-form';
-
-$_class = $prefix_class;
-$_class .= !empty($block_preset) ? ' ' . $prefix_class . '--' . esc_attr($block_preset) : '';
-$_class .= !empty($header_alignment) ? ' is-header-'.  $header_alignment : '';
-$_class .= !empty($class) ? ' ' . esc_attr($class) : '';
-
-if (!empty($title) || !empty($description)) {
-  $header = codetot_build_content_block(array(
-    'class' => 'section-header',
-    'alignment' => $header_alignment,
-    'label' => $label,
-    'title' => $title,
-    'description' => $description
-  ), $prefix_class);
-}
+ob_start();
+if (!empty($form_title)) :
+  printf('<p class="modal__title">%s</p>', $form_title);
+endif;
+$header = ob_get_clean();
 
 ob_start();
-if (!empty($content)) :
-  echo '<div class="'. $prefix_class .'__content">'. $content .'</div>';
-endif;
-
 if (!empty($select_form)) :
-  echo '<div class="' . $prefix_class . '__form default-section__col">';
-  echo do_shortcode('[gravityform id="' . $select_form . '" title="true" description="false" ajax="true"]');
+  echo '<div class="wysiwyg modal__form">';
+  echo do_shortcode('[gravityform id="' . $select_form . '" title="false" description="false" ajax="true"]');
   echo '</div>';
 endif;
-
 $content = ob_get_clean();
 
-if (!empty($select_form)) :
-    the_block('default-section', array(
-      'class' => $_class,
-      'attributes' => ' data-action-attribute="'.$action_attribute.'" data-ct-block="popup-form"',
-      'header' => (!empty($title) || !empty($description)) ? $header : false,
-      'content' => $content
-    ));
-endif;
+the_block('modal', array(
+  'attributes' => ' data-ct-block="popup-form"',
+  'class' => 'modal--popup-form',
+  'id' => esc_attr($form_id),
+  'header' => !empty($header) ? $header : '',
+  'content' => $content
+));
+
 ?>
