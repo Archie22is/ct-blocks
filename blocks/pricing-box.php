02bb0a4085ea609a0f1a261f5979efef5cd28b51
Nguyen Lap <lap@codetot.com>
2021-07-15 17:10:10 +0700

    Fix pricing table add icon to list feature

--- blocks/pricing-box.php
+++ blocks/pricing-box.php
@@ -2,6 +2,30 @@
 $_class = 'f fdc pricing-box';
 $_class .= !empty($class) ? ' ' . $class : '';
 $_highlight_text = !empty($highlight_text) ? $highlight_text : esc_html__('Popular', 'ct-blocks');
+$_has_icon = ($icon_style != 'no-style') ? 'has-icon' : '';
+$_icon_style = !empty($icon_style) ? 'icon--' . $icon_style : 'icon--no-style';
+
+switch ($icon_style) {
+  case 'arrow-right':
+    $icon = codetot_svg('arrow-right', false);
+  break;
+
+  case 'angle-right':
+    $icon = codetot_svg('angle-right', false);
+  break;
+
+  case 'checked-red':
+  case 'checked-green':
+  case 'checked-blue':
+  case 'checked-dark':
+  case 'checked-white':
+    $icon = codetot_svg('checked', false);
+  break;
+
+  default:
+    $icon = '';
+  break;
+}
 ?>
 <div class="<?php echo $_class; ?>">
   <?php if (!empty($title)) : ?>
@@ -24,8 +48,18 @@ $_highlight_text = !empty($highlight_text) ? $highlight_text : esc_html__('Popul
     </div>
   <?php endif; ?>
   <?php if (!empty($description)) : ?>
-    <div class="wysiwyg pricing-box__description">
-      <?php echo $description; ?>
+    <div class="wysiwyg pricing-box__description <?php echo $_has_icon; ?>">
+      <?php
+        if ($icon_style != 'no-style'):
+          $str = $description;
+          $partern = '/<li>(.*?)<\/li>/';
+          $subst = "<li class=\"pricing-box__feature\"><span class=\"pricing-box__icon $_icon_style \">$icon</span><span>$1</span></li>\n";
+          $result = preg_replace($partern, $subst, $str);
+          echo $result;
+        else:
+          echo $description;
+        endif;
+      ?>
     </div>
   <?php endif; ?>
   <?php if (!empty($button_text) && !empty($button_url)) : ?>
