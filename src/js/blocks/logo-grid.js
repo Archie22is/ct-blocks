daddceb48ff34a2219979e39e0232b213dc60391
codetot-thao <72177080+codetot-thao@users.noreply.github.com>
2021-07-21 14:19:09 +0700

    Revert "Update from develop"

--- src/js/blocks/logo-grid.js
+++ src/js/blocks/logo-grid.js
@@ -1,29 +1,40 @@
-import { select, addClass, hasClass } from 'lib/dom'
+import { select, selectAll, addClass, hasClass } from 'lib/dom'
+import { map } from 'lib/utils'
 import carousel from 'lib/carousel'
+
+const SLIDER_READY_CLASS = 'is-ready'
 
 export default el => {
   const sliderEl = select('.js-slider', el)
-  const imageEl = select('.image__img.lazyload', el)
+  const imageEls = selectAll('.image__img', el)
   // eslint-disable-next-line no-unused-vars
   let slider = null
 
   const customOptions = {
     on: {
       ready: function () {
-        if (imageEl) {
-          const timer = setInterval(() => {
-            if (hasClass('lazyloaded', imageEl)) {
-              slider.resize()
-              addClass('is-ready', sliderEl)
-              clearInterval(timer)
-            }
-          }, 500)
-        }
+        addClass(SLIDER_READY_CLASS, sliderEl)
       }
     }
   }
 
   if (sliderEl) {
-    slider = carousel(sliderEl, customOptions)
+    if (imageEls.length) {
+      const timer = setInterval(() => {
+        let count = 0
+
+        map(imageEl => {
+          if (hasClass('lazyloaded', imageEl)) {
+            count++
+          }
+        }, imageEls)
+
+        if (count === imageEls.length) {
+          slider = carousel(sliderEl, customOptions)
+
+          clearInterval(timer)
+        }
+      }, 300)
+    }
   }
 }
