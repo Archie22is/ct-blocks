d1c9865e7d9845841fa5539e774b8f0766d09da2
Khoi Nguyen <khoi@codetot.com>
2021-08-05 18:12:02 +0700

    Add icon svg and icon image to counters block (WIP)

--- blocks/counters-item.php
+++ blocks/counters-item.php
@@ -1,5 +1,18 @@
 <?php if (!empty($item)) : ?>
   <div class="f fdc counters__inner">
+    <?php if (!empty($item['icon']) || !empty($item['icon_image'])) : ?>
+      <div class="mb-05 counters__wrapper">
+        <?php if ($item['icon_type'] === 'svg') : ?>
+          <span class="counters__svg" aria-hidden="true"><?php echo $item['icon_svg']; ?></span>
+        <?php elseif ($item['icon_type'] === 'image' && !empty($item['icon'])) :
+          the_block('image', array(
+            'image' => $item['icon_image'],
+            'size' => 'medium',
+            'class' => 'image--contain counters__image'
+          ));
+        endif; ?>
+      </div>
+    <?php endif; ?>
     <?php if (!empty($item['count'])) : ?>
       <p class="counters__count">
         <span class="h3 counters__count-number js-counter"><?php echo $item['count']; ?></span>
