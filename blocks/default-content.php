308cfcfb22d8b1e0f08b58673c5f0c5f0ffa208e
Khoi Nguyen <khoi@codetot.com>
2021-08-13 14:59:51 +0700

    Add background contract, fix css

--- blocks/default-content.php
+++ blocks/default-content.php
@@ -1,20 +1,19 @@
 <?php
 $_class = 'default-content';
-$_class .= !empty($class) ? ' ' . $class : '';
 $_class .= !empty($content_alignment) ? ' is-content-alignment-' . esc_attr($content_alignment) : ' is-content-alignment-left';
 $_class .= !empty($background_type) ? codetot_generate_block_background_class($background_type) : ' section';
+$_class .= !empty($background_contract) ? ' is-' . $background_contract . '-contract' : '';
+$_class .= !empty($class) ? ' ' . $class : '';
 
 $header = codetot_build_content_block(array(
   'title' => !empty($title) ? $title : '',
-  'title_tag' => 'h2'
+  'title_tag' => !empty($section_title_tag) ? $section_title_tag : 'h2'
 ), 'default-content');
 
 ob_start();
   if( !empty($content) ):
-  echo '<div class="wysiwyg default-content__description">';
-  echo $content;
-  echo '</div>';
-endif;
+    printf('<div class="wysiwyg default-content__description">%s</div>', $content);
+  endif;
 $content = ob_get_clean();
 
 the_block('default-section', array(
