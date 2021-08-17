32ead86e4981c0505e34a4ea5ec0e21987ea150a
Khoi Nguyen <khoi@codetot.com>
2021-08-17 08:00:24 +0700

    Refactor hero link to lazyload from second slide, using same lib carousel

--- blocks/hero-link.php
+++ blocks/hero-link.php
@@ -7,34 +7,34 @@ $_class .= !empty($enable_page_dots) ? ' hero-link--dots-' . esc_attr($page_dots
 
 $id = !empty($anchor_name) ? $anchor_name : '';
 $carousel_settings = array(
-  'prevNextButtons' => !empty($enable_prev_next_buttons) ? true : false,
-  'pageDots' => !empty($enable_page_dots) ? true : false,
+  'prevNextButtons' => !empty($enable_prev_next_buttons),
+  'pageDots' => !empty($enable_page_dots),
   'cellAlign' => 'center',
   'groupCells' => 1,
   'wrapAround' => true,
-  'autoPlay' => 3500,
+  'autoPlay' => !empty($slider_autoplay) ? $slider_autoplay: false
 );
 
 if(!empty($items)) : ?>
 <div class="<?php echo $_class;?>" <?php if(!empty($id)) : ?> id="<?php echo $id; ?>"<?php endif; ?> data-ct-block="hero-link" data-reveal="fade-up">
   <div class="hero-link__inner">
-    <div class="hero-link__slider js-slider" <?php if (!empty($carousel_settings)) : ?> data-options='<?= json_encode($carousel_settings); ?>' <?php endif; ?>>
+    <div class="hero-link__slider js-slider"  <?php if (!empty($carousel_settings)) : ?> data-carousel='<?= json_encode($carousel_settings); ?>' <?php endif; ?>>
       <?php foreach($items as $key => $item) : ?>
         <div class="hero-link__item<?php if ($key > 0) : ?> is-not-loaded<?php endif; ?>">
           <?php if ($key > 0) : ?><noscript><?php endif; ?>
             <div class="hero-link__item-inner">
               <?php
+              $image_html = get_block('image', array(
+                'image' => $item['image']['ID'],
+                'class' => 'image--cover hero-link__image js-image',
+                'lazyload' => false,
+                'size' => wp_is_mobile() ? 'medium' : 'large'
+              ));
+
               if($item['url']) {
-                echo '<a class="hero-link__url" href="'.$item['url'].'" target="'.$item['button_target'].'">';
-              }
-                the_block('image', array(
-                  'image' => $item['image']['ID'],
-                  'class' => 'image--cover hero-link__image js-image',
-                  'lazyload' => false,
-                  'size' => wp_is_mobile() ? 'medium' : 'large'
-                ));
-              if($item['url']) {
-                echo '</a>';
+                printf('<a class="hero-link__url" href="%1$s" target="%2$s">%3$s</a>', $item['url'], $item['button_target'], $image_html);
+              } else {
+                echo $image_html;
               }
               ?>
             </div>
