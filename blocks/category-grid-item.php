a495b8322c382287259c82663d29c51d7f9b5cf4
Khoi Nguyen <khoi@codetot.com>
2021-08-17 14:26:15 +0700

    Update category grid,  add content aligment and image_size attribute

--- blocks/category-grid-item.php
+++ blocks/category-grid-item.php
@@ -1,6 +1,8 @@
 <?php if (!empty($category) && is_object($category)) :
   $thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true);
   $placeholder_image = wc_placeholder_img_src();
+  $_image_class = !empty($image_size) ? 'image--' . esc_html($image_size) : ' image--cover';
+  $_image_class .= !empty($image_size) && $image_size !== 'default' ? ' image--hd' : '';
   ?>
   <div class="category-grid__item">
     <a class="category-grid__link" href="<?php echo get_term_link($category->term_id); ?>" title="<?php printf('View %s', $category->name); ?>">
@@ -8,7 +10,7 @@
       if (!empty($thumbnail_id)) :
         the_block('image', array(
           'image' => $thumbnail_id,
-          'class' => 'image--cover image--hd category-grid__image'
+          'class' => sprintf('%s category-grid__image', $_image_class)
         ));
       else :
         the_block('image-placeholder', array(
