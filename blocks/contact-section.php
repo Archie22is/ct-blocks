a290677c7aa7ca295daff51ccb18caf391255f00
Khoi Nguyen <khoi@codetot.com>
2021-08-18 17:49:28 +0700

    Refactor block contact-section (WIP - Need resolve)

--- blocks/contact-section.php
+++ blocks/contact-section.php
@@ -1,5 +1,4 @@
 <?php
-$container = codetot_site_container();
 
 $_class = 'contact-section';
 $_class .= !empty($contact_primary_layout) ? ' contact-section--primary-' . $contact_primary_layout : '';
@@ -9,73 +8,68 @@ $_class .= !empty($contact_form_style) ? ' contact-section--' . $contact_form_st
 $_class .= !empty($background_image) ? ' has-contact-section-background' : '';
 $_class .= !empty($class) ? ' ' . $class : '';
 
-?>
-<section class="<?php echo $_class; ?>" data-ct-block="contact-section">
-  <div class="<?php echo $container; ?> contact-section__container">
-    <div class="grid contact-section__grid contact-section__grid--primary">
-      <div class="grid__col contact-section__col contact-section__col--map">
-        <?php if (!empty($google_maps)) : ?>
-          <div class="contact-section__map-wrapper">
-            <div class="contact-section__map js-map" data-default-zoom="14" data-clicked-zoom="18">
-              <div class="contact-section__map-marker js-marker"
-                   data-lat="<?php echo $google_maps['lat']; ?>"
-                   data-lng="<?php echo $google_maps['lng']; ?>"
-                   data-marker="<?php echo get_template_directory_uri(); ?>/assets/img/marker.png">
-                <div class="contact-section__map-popup">
-                  <p class="contact-section__map-address"><?php echo $google_maps['address']; ?></p>
-                  <div class="contact-section__loader"></div>
+ob_start(); ?>
+<div class="grid contact-section__grid contact-section__grid--primary">
+  <div class="grid__col contact-section__col contact-section__col--map">
+    <?php if (!empty($google_maps)) : ?>
+      <div class="contact-section__map-wrapper">
+        <div class="contact-section__map js-map" data-default-zoom="14" data-clicked-zoom="18">
+          <div class="contact-section__map-marker js-marker"
+                data-lat="<?php echo $google_maps['lat']; ?>"
+                data-lng="<?php echo $google_maps['lng']; ?>"
+                data-marker="<?php echo get_template_directory_uri(); ?>/assets/img/marker.png">
+            <div class="contact-section__map-popup">
+              <p class="contact-section__map-address"><?php echo $google_maps['address']; ?></p>
+              <div class="contact-section__loader"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
+  </div>
+  <div class="grid__col contact-section__col contact-section__col--block">
+    <div class="contact-section__wrapper">
+      <!-- Form vs Content -->
+      <div class="grid contact-section__grid contact-section__grid--secondary">
+        <div class="grid__col contact-section__col contact-section__col--content">
+          <div class="contact-section__content">
+            <div class="grid contact-section__grid contact-section__grid--tertiary">
+              <?php if (!empty($address)) : ?>
+                <div class="grid__col contact-section__col contact-section__col--address">
+                  <div class="wysiwyg contact-section__inner">
+                    <?php echo $address; ?>
+                  </div>
                 </div>
-              </div>
+              <?php endif; ?>
+              <?php if (!empty($contact_information)) : ?>
+                <div class="grid__col contact-section__col contact-section__col--info">
+                  <div class="wysiwyg contact-section__inner">
+                    <?php echo $contact_information; ?>
+                  </div>
+                </div>
+              <?php endif; ?>
+            </div>
+          </div>
+        </div>
+        <?php if (!empty($contact_form)) : ?>
+          <div class="grid__col contact-section__col contact-section__col--form">
+            <div
+              class="contact-section__form">
+              <?php echo do_shortcode('[gravityform id="' . $contact_form . '" title="true" description="false" ajax="true"]'); ?>
             </div>
           </div>
         <?php endif; ?>
       </div>
-      <div class="grid__col contact-section__col contact-section__col--block">
-
-        <div class="contact-section__wrapper">
-          <!-- Form vs Content -->
-          <div class="grid contact-section__grid contact-section__grid--secondary">
-            <div class="grid__col contact-section__col contact-section__col--content">
-              <div class="contact-section__content">
-                <div class="grid contact-section__grid contact-section__grid--tertiary">
-                  <?php if (!empty($address)) : ?>
-                    <div class="grid__col contact-section__col contact-section__col--address">
-                      <div class="wysiwyg contact-section__inner">
-                        <?php echo $address; ?>
-                      </div>
-                    </div>
-                  <?php endif; ?>
-                  <?php if (!empty($contact_information)) : ?>
-                    <div class="grid__col contact-section__col contact-section__col--info">
-                      <div class="wysiwyg contact-section__inner">
-                        <?php echo $contact_information; ?>
-                      </div>
-                    </div>
-                  <?php endif; ?>
-                </div>
-              </div>
-            </div>
-            <?php if (!empty($contact_form)) : ?>
-              <div class="grid__col contact-section__col contact-section__col--form">
-                <div
-                  class="contact-section__form">
-                  <?php echo do_shortcode('[gravityform id="' . $contact_form . '" title="true" description="false" ajax="true"]'); ?>
-                </div>
-              </div>
-            <?php endif; ?>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
-  <?php if (!empty($background_image)) : ?>
-    <div class="contact-section__background-image">
-      <?php the_block('image', array(
-      'class' => 'image--cover js-image',
-      'attributes' => ' data-reveal="fade-up"',
-      'size' => 'logo',
-      'image' => $background_image
-      )); ?>
-    </div>
-  <?php endif; ?>
-</section>
+</div>
+<?php
+$content = ob_get_clean();
+
+the_block('default-section', array(
+  'id' => $anchor_name ?? '',
+  'attributes' => ' data-ct-block="contact-section"',
+  'class' => $_class,
+  'background_image' => $background_image,
+  'content' => $content
+));
