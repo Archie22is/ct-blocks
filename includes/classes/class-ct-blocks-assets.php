bae261c477a0eb2b71ad9bf34925c8a1ee69b2dd
Khoi Nguyen <khoi@codetot.com>
2021-09-28 15:32:28 +0700

    Register assets, fix wrong version

--- includes/classes/class-ct-blocks-assets.php
+++ includes/classes/class-ct-blocks-assets.php
@@ -31,20 +31,23 @@ class Codetot_Blocks_Assets {
   }
 
 	public function __construct() {
-    $this->theme_environment = $this->is_localhost() ? '' : '.min';
+		$this->theme_version = $this->is_localhost() ? substr(sha1(rand()), 0, 6) : CODETOT_BLOCKS_VERSION;
+		$this->theme_environment = $this->is_localhost() ? '' : '.min';
 
     add_action('wp_enqueue_scripts', array($this, 'load_assets'), 10);
 	}
 
   public function load_assets() {
 		if (!$this->is_localhost()) {
-			wp_enqueue_style('ct-blocks-frontend-legacy-style', CODETOT_BLOCKS_PLUGIN_URI . '/assets/css/legacy-frontend.min.css', array(), CODETOT_BLOCKS_VERSION);
+			wp_enqueue_style('ct-blocks-frontend-legacy-style', CODETOT_BLOCKS_PLUGIN_URI . '/assets/css/legacy-frontend.min.css', array(), $this->theme_version);
+			wp_enqueue_style('ct-blocks-frontend-style', CODETOT_BLOCKS_PLUGIN_URI . '/assets/css/frontend.min.css', array(), $this->theme_version);
 		}
 
-		wp_enqueue_script('ct-blocks-frontend-legacy-script', CODETOT_BLOCKS_PLUGIN_URI . '/assets/js/legacy-frontend' . $this->theme_environment . '.js', array('jquery'), CODETOT_BLOCKS_VERSION, true);
-  }
+		wp_enqueue_script('ct-blocks-frontend-legacy-script', CODETOT_BLOCKS_PLUGIN_URI . '/assets/js/legacy-frontend' . $this->theme_environment . '.js', array('jquery'), $this->theme_version, true);
+		wp_enqueue_script('ct-blocks-frontend-script', CODETOT_BLOCKS_PLUGIN_URI . '/assets/js/frontend' . $this->theme_environment . '.js', array(), $this->theme_version, true);
+	}
 
   public function is_localhost() {
-    return !empty($_SERVER['HTTP_X_CODETOT_BLOCK_HEADER']) && $_SERVER['HTTP_X_CODETOT_BLOCK_HEADER'] === 'development';
+    return !empty($_SERVER['HTTP_X_CODETOT_BLOCKS_HEADER']) && $_SERVER['HTTP_X_CODETOT_BLOCKS_HEADER'] === 'development';
   }
 }
