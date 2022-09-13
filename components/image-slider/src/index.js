7f8a6e555f1fb73e7aee6af4daa9c10458de12e7
CODE TOT JSC <dev@codetot.com>
2022-09-13 09:30:27 +0000

    Prepare production package

--- components/image-slider/src/index.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
-import Edit from './edit';
-import save from './save';
-registerBlockType( 'ct-blocks/image-slider', {
-	edit: Edit,
-	save,
-} );
